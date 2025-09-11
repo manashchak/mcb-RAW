6a628fc209c34e2e9ac020928ea17380db0a1641
Manash Chakraborty <112432090+manashchak@users.noreply.github.com>
2025-09-11 09:50:20 -0700

    PDF Chat. Upto: Text Split

--- Ai Projects/Multi-modal RAG with LangChain/PDF Chat/app.py
+++ Ai Projects/Multi-modal RAG with LangChain/PDF Chat/app.py
@@ -1,9 +1,48 @@
 #%% imports
-
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
 
 #%% Main
+
+### For Testing______________________________
+# def main():
+#     print("Main: Under Development")
+#     load_dotenv()
+#     print(os.getenv("OLLAMA_BASE_URL"))
+#     print(os.getenv("OLLAMA_CHAT_MODEL"))
+
 def main():
-    print("Main: Under Development")
+    load_dotenv()
+    st.set_page_config(page_title="Single PDF Database")
+    st.header("Ask Your PDF")
+
+    #* Uploading the PDF file
+    pdf = st.file_uploader("Upload Your PDF Here", type="pdf")
+    
+    #* Extracting PDF
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()        
+        # st.write(text)  # DEBUG
+        
+        #* split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+        st.write(chunks)  # DEBUG
+
+
 
 if __name__ == '__main__':
     main()
+
+# %%
