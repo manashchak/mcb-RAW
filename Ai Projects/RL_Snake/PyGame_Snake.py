4ebae86c4d89f5c37807e1808f187f2b516f0e6f
Manash Chakraborty <112432090+manashchak@users.noreply.github.com>
2025-09-18 10:36:31 -0700

    PyGame_Snake.py: collisions

--- Ai Projects/RL_Snake/PyGame_Snake.py
+++ Ai Projects/RL_Snake/PyGame_Snake.py
@@ -10,9 +10,9 @@ GREEN = (173, 204, 96)
 DARK_GREEN = (43, 51, 24)
 
 class Food:
-    def __init__(self):
+    def __init__(self, snake_body):
         # self.position = Vector2(5,6) # Vector2 class of Pygame is usefull in this case. 6th column, 7th row.
-        self.position = self.generate_random_pos()
+        self.position = self.generate_random_pos(snake_body)
 
     def draw(self):
         #? Displace Surface: all the game objects are here. One per game
@@ -22,10 +22,17 @@ class Food:
         # pg.draw.rect(screen, DARK_GREEN, food_rect ) # (surface, color, rect object)
         screen.blit(food_texture, food_rect.topleft) # (surface, rect object)
     
-    def generate_random_pos(self):
+    def generate_random_cell(self):
         x = random.randint(0, number_of_cells-1)
         y = random.randint(0, number_of_cells-1)
         position = Vector2(x,y)
+        return position
+
+    def generate_random_pos(self, snake_body):
+        position = self.generate_random_cell()
+        #? make sure the generated food location is not coinciding with snake_body location
+        while position in snake_body:
+            position = self.generate_random_cell()
         return position
 
 class Snake:
@@ -33,6 +40,7 @@ class Snake:
         #? snake body = [ head, [tail] ]
         self.body = [Vector2(6,9), Vector2(5,9), Vector2(4,9)]
         self.direction = Vector2(1,0) # to move to the right
+        self.add_segment = False
 
     def draw(self):
         for segment in self.body:
@@ -40,30 +48,61 @@ class Snake:
             pg.draw.rect(screen, DARK_GREEN, segment_rect, 0, 7) # (surface, color, rect object, <filled with color>, <border>)
     
     def update(self):
-        self.body = self.body[:-1] # removed one from the tail
-        self.body.insert(0, self.body[0]+self.direction) # at 0th index add direction to the head. 
+        self.body.insert(0, self.body[0]+self.direction) # increase the snake body in it's head direction. at 0th index add direction to the head. 
+        if self.add_segment == True:
+            self.add_segment = False
+        else:
+            self.body = self.body[:-1] # removed one from the tail. 
+    
+    def reset(self):
+        self.body = [Vector2(6,9), Vector2(5,9), Vector2(4,9)]
 
 class Game:
     def __init__(self):
         self.snake = Snake()
-        self.food  = Food()
+        self.food  = Food(self.snake.body)
+        self.state = "RUNNING"
 
     def draw(self):
         self.food.draw()
         self.snake.draw()
 
     def update(self):
-        self.snake.update()
+        if self.state == "RUNNING":
+            self.snake.update()
+            self.check_collision_with_food()
+            self.check_collision_with_edges()
+            self.check_collision_with_tail()
     
     def check_collision_with_food(self):
         if self.snake.body[0] == self.food.position:
-            print("Eating Food")
+            self.food.position = self.food.generate_random_pos(self.snake.body)
+            self.snake.add_segment = True
+    
+    def check_collision_with_edges(self):
+        if self.snake.body[0].x == number_of_cells or self.snake.body[0].x == -1: # snake passed the right edge or left edge
+            self.game_over()
+        if self.snake.body[0].y == number_of_cells or self.snake.body[0].y == -1: # snake passed the bottom or top
+            self.game_over()
+    
+    def check_collision_with_tail(self):
+        headless_body = self.snake.body[1:]
+        if self.snake.body[0] in headless_body:
+            self.game_over()
 
+    def game_over(self):
+        print("Game Over")
+        self.snake.reset()
+        self.food.generate_random_pos(self.snake.body)
+        self.state = "STOPPED"
 
 #* Canvas
 cell_size = 30
 number_of_cells = 25
-screen = pg.display.set_mode((cell_size*number_of_cells, cell_size*number_of_cells)) # origin-> top-left.
+OFFSET = 75
+OFFSET_BORDER = 5
+
+screen = pg.display.set_mode((2*OFFSET + cell_size*number_of_cells, 2*OFFSET + cell_size*number_of_cells)) # origin-> top-left.
 pg.display.set_caption("Retro Snake")
 
 clock = pg.time.Clock()
@@ -94,6 +133,9 @@ while True:
         
         #? Make sure snake doesn't turn 180 degree !
         if event.type == pg.KEYDOWN:
+            if game.state == "STOPPED":
+                game.state = "RUNNING"
+
             if event.key == pg.K_UP and game.snake.direction != Vector2(0,1): #? if key=UP and snake is not moving downwards.
                 game.snake.direction = Vector2(0, -1) # move up
             if event.key == pg.K_DOWN and game.snake.direction != Vector2(0,-1): #? if key=DOWN and snake is not moving downwards.
@@ -105,6 +147,7 @@ while True:
 
     # Drawing
     screen.fill(GREEN)  # Fill the display surface.
+    pg.draw.rect(screen, DARK_GREEN, (OFFSET-5, OFFSET-5, cell_size*number_of_cells+10, cell_size*number_of_cells+10), 5) # (surface, color, rect, border_size)
     game.draw()
 
     pg.display.update() # takes all the changes and draws updates
