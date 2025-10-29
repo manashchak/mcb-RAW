97cef7d72c6309175acdc56659e721e03697d7ce
Manash Chakraborty <112432090+manashchak@users.noreply.github.com>
2025-10-29 14:37:47 -0700

    +U requirements.txt

--- Learnings/STTS/validations.py
+++ Learnings/STTS/validations.py
@@ -1,6 +1,6 @@
 from faster_whisper import WhisperModel
 
-model_size = "small.en"
+model_size = "medium.en"
 
 # Run on GPU with FP16
 # model = WhisperModel(model_size, device="cuda", compute_type="float16")
@@ -10,9 +10,11 @@ model_size = "small.en"
 # or run on CPU with INT8
 model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
-segments, info = model.transcribe("assets/hotwords.mp3", beam_size=5)
+segments, info = model.transcribe("C:/MCB Local/Manash-Git/mcb-RAW/Learnings/STTS/assets/hotwords.mp3", beam_size=5)
 
 print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
 for segment in segments:
     print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+
+
